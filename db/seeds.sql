e26c9c17814d7d36d8a56bc3df6da87f8e9e1f47
Preston <preston159@gmail.com>
2020-01-11 15:19:09 -0700

    got basic functionality.  Still needs more work on styling

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Krabby Patty', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('ChumBurger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pretty Patty', TRUE);
